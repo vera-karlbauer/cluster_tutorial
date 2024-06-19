0348aee518f63ec31d5c44334115769c557be037
Vera N. Karlbauer <vera@karlbauer.org>
2024-06-19 16:33:45 +0200

    Rewritten using base R

--- 02_Analysis/01_Scripts/myscript.R
+++ 02_Analysis/01_Scripts/myscript.R
@@ -2,24 +2,16 @@
 # Author: Vera N. Karlbauer
 # Contact: vera_karlbauer@psych.mpg.de
 # Date created: 19-06-2024
-# What this script does: Imports mtcars data, gets xxx means, exports output
+# What this script does: Imports mtcars data, gets summary stats, exports output
 # To be run on psycl via run_myscript.sh
 
 ## Setup
-# load packages
-install.packages("tibble")
-library(dplyr)
-library(tibble)
-
 # import data
-mydata <- read.csv("01_Data/02_Clean/testdata.csv", col.names = TRUE)
-mydata <- mydata %>%
-  rownames_to_column(var = "cartype")
+mydata <- read.csv("01_Data/02_Clean/testdata.csv")
 
 ## Get mean mpg for each nr of gears
-mydata <- mydata %>%
-  group_by(gear) %>%
-  summarise(mean_mpg = mean(mpg))
+mydata <- aggregate(mpg ~ gear, data = mydata, FUN = mean)
+names(mydata)[names(mydata) == "mpg"] <- "mean_mpg"
 
 ## Export data
 write.csv(mydata, file = "02_Analysis/02_Results/testoutput.csv", row.names = FALSE)
