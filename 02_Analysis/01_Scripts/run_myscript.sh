7d5311e9b81fdf825e5f27751072bdba1d94ac27
Vera N. Karlbauer <vera@karlbauer.org>
2024-06-20 15:52:52 +0200

    Remoced node specification

--- 02_Analysis/01_Scripts/run_myscript.sh
+++ 02_Analysis/01_Scripts/run_myscript.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/bash
 #SBATCH --job-name=myjob    # Name of the job showing up in the queue
-#SBATCH -w psycl02	# Specify node (optional), this runs the job on psycl02
 #SBATCH --ntasks=1         # Nr of CPUs to run job on, e.g. 1 CPU
 #SBATCH --mem=10g        # Job memory request, e.g. 10g 
 #SBATCH --time=1:00:00    # Time limit hrs:min:sec, this lets it run for 1h
